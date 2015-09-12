1f01194a439fc44bd41f8e7c8fcb120804f80e9c
陈为 <chenwei2000121@163.com>
2015-09-12 09:02:05 +0800

    initial commit

---
+++ HoldToSaveImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HoldToSaveImage
+//
+//  Created by 陈为 on 9/12/15.
+//  Copyright © 2015 David Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
